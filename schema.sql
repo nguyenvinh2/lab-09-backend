3c49a812ef77fb4b0a71fa2a969ff759aac1a527
Vinh Nguyen <nguyenv2@outlook.com>
2019-05-03 11:03:32 -0700

    cache invalidation works

--- schema.sql
+++ schema.sql
@@ -1,23 +1,20 @@
-DROP TABLE IF EXISTS weathers;
-DROP TABLE IF EXISTS events;
-DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS weathers, events, locations;
 
 CREATE TABLE IF NOT EXISTS locations ( 
   id SERIAL PRIMARY KEY, 
   search_query VARCHAR(255), 
   formatted_query VARCHAR(255), 
-  latitude NUMERIC(8, 6), 
-  longitude NUMERIC(9, 6),
+  latitude DECIMAL, 
+  longitude DECIMAL,
   created_at BIGINT
 );
 
 CREATE TABLE IF NOT EXISTS weathers ( 
   id SERIAL PRIMARY KEY, 
-  forecast VARCHAR(255), 
+  forecast TEXT, 
   time VARCHAR(255), 
   created_at BIGINT,
-  location_id INTEGER,
-  location VARCHAR(255)
+  location_id INTEGER NOT NULL REFERENCES locations(id) 
 );
 
 CREATE TABLE IF NOT EXISTS events (
@@ -27,6 +24,5 @@ CREATE TABLE IF NOT EXISTS events (
   event_date VARCHAR(255),
   summary TEXT,
   created_at BIGINT,
-  location_id INTEGER,
-  location VARCHAR(255)
+  location_id INTEGER NOT NULL REFERENCES locations(id) 
 );
